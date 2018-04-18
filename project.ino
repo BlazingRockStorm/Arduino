bea1e074a754d07b550c2f7d0a9a0ba5d3f7069b
BlazingRockStorm <rocker.gryphon@gmail.com>
2018-04-18 19:24:24 +0700

    debug. all sensor now fully operatable

--- project.ino
+++ project.ino
@@ -14,14 +14,17 @@ int checkFire(int x, int y){
 }
 
 void alarm(int sensor1,int sensor2,int sensor3){
-  if (checkFire(sensor1, 400)==1){
+  if (checkFire(400, sensor1)==1){
     digitalWrite(led,HIGH);
-  } else if (checkFire(90, sensor2)==1){
+    Serial.println("Có cháy");
+  } else if (checkFire(sensor2, 300)==1){
     digitalWrite(led,HIGH);
-  } else if (checkFire(sensor3, 500)==1){
+    Serial.println("Có cháy");
+  } else if (checkFire(sensor3, 720)==1){
     digitalWrite(led,HIGH);
+    Serial.println("Có cháy");
   } else {
-    digitalWrite(led,LOW);    
+    digitalWrite(led,LOW);
   }
 }
 
@@ -30,10 +33,14 @@ void loop() {
   int lightSensor = analogRead(A1);   //đọc giá trị điện áp ở chân A1 - chân cảm biến
   int heatSensor = analogRead(A2);
                                 //(value luôn nằm trong khoảng 0-1023)
-  Serial.println(gasSensor);        //xuất ra giá trị vừa đọc
+//  Serial.println(checkFire(gasSensor, 400));        //xuất ra giá trị vừa đọc 
+//  Serial.println(checkFire(90, lightSensor));        //xuất ra giá trị vừa đọc 
+//  Serial.println(checkFire(heatSensor, 500));
+//  Serial.println(heatSensor);
+  Serial.println(lightSensor);
   //-----------------------------------------------------
   delay(1000);           //đợi 1 giây để bạn kịp tháy serial - (optional)
-//  alarm(gasSensor,lightSensor,heatSensor);
+  alarm(gasSensor,lightSensor,heatSensor);
 }
 
 //Cảm biến sẽ chênh lệch giá trị 200 khi có thay đổi ánh sáng => chênh lệch giá trị khoảng 200 thì báo cháy luôn
