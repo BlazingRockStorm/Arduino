6b278a8daf155a1b1d0b9d6f9a36004f6b4a57f6
BlazingRockStorm <rocker.gryphon@gmail.com>
2018-04-18 20:35:16 +0700

    add speaker. finish the project

--- project.ino
+++ project.ino
@@ -1,7 +1,9 @@
 #define led 13
+int pinSpeaker = 3; 
 
 void setup() {
   pinMode(led,OUTPUT);
+  pinMode(pinSpeaker, OUTPUT);
   Serial.begin(9600);  //Mở cổng Serial để giap tiếp | tham khảo Serial
 }
 
@@ -16,15 +18,34 @@ int checkFire(int x, int y){
 void alarm(int sensor1,int sensor2,int sensor3){
   if (checkFire(400, sensor1)==1){
     digitalWrite(led,HIGH);
+    playTone(300, 160); 
     Serial.println("Có cháy");
   } else if (checkFire(sensor2, 300)==1){
     digitalWrite(led,HIGH);
+    playTone(300, 160); 
     Serial.println("Có cháy");
   } else if (checkFire(sensor3, 720)==1){
     digitalWrite(led,HIGH);
+    playTone(300, 160); 
     Serial.println("Có cháy");
   } else {
     digitalWrite(led,LOW);
+    playTone(0, 0); 
+  }
+}
+
+void playTone(long duration, int freq)
+{
+  duration *= 1000;
+  int period = (1.0 / freq) * 1000000;
+  long elapsed_time = 0;
+  while (elapsed_time < duration)
+  {
+    digitalWrite(pinSpeaker,HIGH);
+    delayMicroseconds(period / 2);
+    digitalWrite(pinSpeaker, LOW);
+    delayMicroseconds(period / 2);
+    elapsed_time += (period);
   }
 }
 
@@ -39,7 +60,7 @@ void loop() {
 //  Serial.println(heatSensor);
   Serial.println(lightSensor);
   //-----------------------------------------------------
-  delay(1000);           //đợi 1 giây để bạn kịp tháy serial - (optional)
+  delay(100);           //đợi 1 giây để bạn kịp tháy serial - (optional)
   alarm(gasSensor,lightSensor,heatSensor);
 }
 
